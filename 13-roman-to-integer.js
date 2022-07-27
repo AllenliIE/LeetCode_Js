76d6e9bfc368b7b8d691d3ceae8bdf0f586b1592
AllenliIE <allen.li.service@gmail.com>
2022-07-28 00:56:08 +0800

    feat: 13-roman-to-integer.js

---
+++ 13-roman-to-integer.js
@@ -0,0 +1,60 @@
+//Blog:http://52.198.119.162/2022/05/10/leetcode-js-13-roman-to-integer/
+
+var romanToInt = function (s) {
+  const romanMap = { //建立羅馬轉換表
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+
+  let total = 0 //宣告待數字加總的空間
+
+  for (let i = 0; i < s.length; i++) { //input字串決定i迴圈次數
+    if (romanMap[s[i]] < romanMap[s[i + 1]]) { //如果 i 小於 i+1
+      total += romanMap[s[i + 1]] - romanMap[s[i]] //羅馬數字的規則，後面數字比前面大，須將後面減前面值
+      i++ //i進到下一個順位
+    } else {
+      total += romanMap[s[i]]
+    }
+  }
+  return total
+}
+
+// i = for loop
+// s.length = 7("MCMXCIV")
+
+// step.1
+// i = 0
+// romanMap[s[i]] = "M" = 1000
+// romanMap[s[i + 1]] = "C" = 100
+// else { total = 0 + 1000 = 1000 }
+
+// step.2
+// i = 1
+// romanMap[s[i]] = "C" = 100
+// romanMap[s[i + 1]] = "M" = 1000
+// if (100 < 1000) { total = 1000 + 1000 - 100 = 1900 }
+// i++
+
+// step.3
+// i = 3
+// romanMap[s[i]] = "X" = 10
+// romanMap[s[i + 1]] = "C" = 100
+// if (10 < 100) { total = 1900 + 100 - 10 = 1990 }
+// i++
+
+// step.4
+// i = 5
+// romanMap[s[i]] = "I" = 1
+// romanMap[s[i + 1]] = "V" = 5
+// if (1 < 5) { total = 1990 + 5 - 1 = 1994 }
+// i++
+
+// step.5
+// i = 7
+// for i(7) < s.length(7) //條件不成立
+// break
