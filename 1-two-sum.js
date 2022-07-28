9eee2b275bb4758f63f3b7761ab708b8ebda3dc0
AllenliIE <allen.li.service@gmail.com>
2022-07-29 01:17:45 +0800

    feat: modify 1-two-sum.js

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,5 +1,13 @@
 //Blog:http://52.198.119.162/2022/05/22/leetcode-js-1-two-sum/
 
+//<strong>Solution:</strong>
+//1. 想像數組的陣列為一區塊
+//2. 用「for迴圈」將目標值與陣列依序判斷
+//3.「if條件」將目標值減去數組中的數字並判斷是否未定義
+//   true: 不是未定義，回傳[對應box中值的位置, 回合數]
+//   false: 是未定義，將比對回合數放入box中對應數組位置
+//ex.詳情請閱FlowChart
+
 const twoSum = function (nums, target) {
   const box = {};
   for (let i = 0; i < nums.length; i++) {
@@ -12,17 +20,20 @@ const twoSum = function (nums, target) {
 
 };
 
-// Input: nums = [2, 7, 11, 15], target = 9
-// i = 0 1  2  3
-// step.1
-// box = []
-// i = 0; nums[i] = 2;
-// if ((box[9 - 2]) === undefined) // box[7]沒有東西
-//   box[nums[i]] = i => box[2] = 0 // box[ , , 0]
-// i = 0 1  2
+/* <strong>Example 1</strong>
+<pre style='background-color:#ggg'>
+Input: nums = [2, 7, 11, 15], target = 9
+i = 0 1  2  3
+step.1
+box = []
+i = 0; nums[i] = 2;
+if ((box[9 - 2]) === undefined) // box[7]沒有東西
+  box[nums[i]] = i => box[2] = 0 // box[ , , 0]
+i = 0 1  2
 
-// step.2
-// box = [, , 0]
-// i = 1; nums[1] = 7;
-// if ((box[9 - 7]) !== undefined) // box[2] = 0
-//   return ([box[9 - 7], 1]) // [0, 1]
+step.2
+box = [, , 0]
+i = 1; nums[1] = 7;
+if ((box[9 - 7]) !== undefined) // box[2] = 0
+  return ([box[9 - 7], 1]) // [0, 1]
+</pre>*/
