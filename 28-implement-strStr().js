1c5b62b50631778c5bb197cd754e929279085220
AllenliIE <allen.li.service@gmail.com>
2022-07-30 00:34:25 +0800

    feat: create 28-implement-strStr().js

---
+++ 28-implement-strStr().js
@@ -0,0 +1,40 @@
+//Blog:http://52.198.119.162/2022/06/19/leetcode-js-28-implement-strstr/
+
+//<strong>Solution:</strong>
+//1. if「needle」是空值，回傳0
+//2. if「haystack」是空值或「needle.length」比「haystack.length」大，回傳 - 1
+//3. for迴圈跑haystack的長度，並運用.substr()帶入neeedle進行比較
+//ex.類似excel的MID函式，但.substr()會取陣列起點值，.substring()會取陣列終點值。
+//(A1 = ABCDEF; =MID(A1, 2, 1); B )
+//4.正確回傳陣列起點值，錯誤回傳 - 1
+
+var strStr = function (haystack, needle) {
+  if (!needle) return 0
+  if (!haystack || needle.length > haystack.length) return -1
+
+  for (i = 0; i < haystack.length; i++) {
+    let str = haystack.substr(i, needle.length)
+    if (str === needle) {
+      return i
+    }
+  }
+  return -1
+};
+
+/* <strong>Example 1</strong>
+<pre style='background-color:#ggg'>
+needle.length = 2 //"ll"的長度為兩個字串
+step.1
+haystack = "hello"
+i = 0
+str = haystack.length(0, 2) => "he" !== "ll"
+
+step.2
+i = 1
+str = haystack.length(1, 2) => "el" !== "ll"
+
+step.3
+i = 2
+str = haystack.length(1, 2) => "ll" === "ll"
+return i // i = 2
+</pre> */
