802b191a14bc89893d01d48f850c72bb78d13f7c
AllenliIE <allen.li.service@gmail.com>
2022-08-18 20:11:48 +0800

    feat: create 509-fibonacci-number.js

---
+++ 509-fibonacci-number.js
@@ -0,0 +1,25 @@
+//Blog: http://52.198.119.162/leetcode-js-509-fibonacci-number/
+
+// <strong>Solution:</strong>
+// 1. 判斷 f(0) = 0 為 0, f(1) = 1 為 1。
+// 2. 每一輪的 f(n) 套入 fib function 拆分為 f(1) 與 f(0) 的組合後進行加總。
+
+// <strong>Code.1:</strong>
+var fib = function (n) {
+  if (n <= 0) {
+    return 0
+  } else if (n === 1) {
+    return 1
+  } else {
+    return fib(n - 1) + fib(n - 2)
+  }
+}
+
+/* <strong>Example 1</strong>
+<pre style='background-color:#ggg'>
+Input: n = 2
+
+fib(2 - 1) = fib(1) = 1
+fib(2 - 2) = fib(0) = 0
+return fib(1) + fib(0) = 1
+</pre> */
