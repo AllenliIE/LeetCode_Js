ae2d783e72aa338e3bb006553285c7ace1664e00
AllenliIE <allen.li.service@gmail.com>
2022-09-02 23:57:55 +0800

    feat: create 125-valid-palindrome.js

---
+++ 125-valid-palindrome.js
@@ -0,0 +1,26 @@
+//Blog: http://52.198.119.162/leetcode-js-125-valid-palindrome/
+
+// <strong>Solution:</strong>
+// 1. Input s 轉換成小寫字母。
+// 2. Input s 將特殊符號移除。
+// 3. 宣告 reverseWord 為反轉後的 s。
+// 4. 回傳 s 中符合 reverseWord，完全符合為 0，沒有符合為 -1。
+
+// <strong>Code:</strong>
+var isPalindrome = function(s) {
+  s = s.toLowerCase()
+  s = s.replace(/[^a-z0-9]/ig,"")
+  let reverseWord = s.split("").reverse().join("")
+  
+  return s.indexOf(reverseWord) === 0
+};
+
+/* <strong>FlowChart:</strong>
+<strong>Example 1</strong>
+Input: s = "A man, a plan, a canal: Panama"
+s.toLowerCase() = a man, a plan, a canal: panama
+s.replace = amanaplanacanalpanama
+reverseWord = s.split("").reverse().join("") => amanaplanacanalpanama
+
+return true
+*/
