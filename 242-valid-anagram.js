37b5bbb5e75681efa9b3ba31b630790f4a09f7ca
AllenliIE <allen.li.service@gmail.com>
2022-09-23 19:28:01 +0800

    feat: create 242-valid-anagram.js

---
+++ 242-valid-anagram.js
@@ -0,0 +1,25 @@
+//Blog: http://52.198.119.162/leetcode-js-242-valid-anagram/
+
+// <strong>Solution:</strong>
+// 1. 先判斷 s 和 t 的長度是否不相同，並回傳 false。
+// 2. 將 s 和 t 字串切割，並進行字母排序，再將字母組合。
+// 3. 判斷 s 和 t 是否相同，並回傳true，反之回傳 false。
+
+// <strong>Code:</strong>
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false
+
+  var s = s.split("").sort().join("")
+  var t = t.split("").sort().join("")
+
+  return s === t
+};
+
+/* <strong>FlowChart:</strong>
+<strong>Example 1</strong>
+Input: s = "anagram", t = "nagaram"
+
+s = "aaagmnr"
+t = "aaagmnr"
+
+return true */
