550775a4d7fa59bf7dcadad6014eec7cf9dbf608
AllenliIE <allen.li.service@gmail.com>
2022-09-30 19:43:43 +0800

    feat: create 342-power-of-four.js

---
+++ 342-power-of-four.js
@@ -0,0 +1,28 @@
+//Blog: http://52.198.119.162/leetcode-js-342-power-of-four/
+
+// <strong>Solution:</strong>
+// 1. 先判斷 n 為負數則為 false。
+// 2. 再判斷 n 為 1 時，回傳 true。(4^0 = 1)
+// 3. 運用 while 迴圈，將 n > 5 的數值先行減半。
+// 4. 再回傳 n % 4 的餘數，如為 0 則回傳 true，如為 1 則回傳 false。
+
+// <strong>Code 1:</strongc>
+var isPowerOfFour = function (n) {
+  if (n <= 0) return false
+  if (n === 1) return true
+
+  while (n > 5) {
+    n = n / 4
+  }
+
+  return n % 4 === 0
+};
+
+/* <strong>FlowChart:</strong>
+<strong>Example 1</strong>
+<pre style='background-color:#ggg'>
+Input: n = 16
+
+while (16 > 5) {n = 16 / 4} //n = 4
+return 4 % 4 === 0 //true
+</pre> */
