0744b176ea2edf6c9ffc32f9478d2bb723624d36
AllenliIE <allen.li.service@gmail.com>
2022-11-03 23:59:32 +0800

    feat: modify 8-string-to-integer.js

--- 8-string-to-integer.js
+++ 8-string-to-integer.js
@@ -12,13 +12,12 @@
 //  詳請閱FlowChart
 
 var myAtoi = function (s) {
-  // "   -321321 bunnies" 
-  //ignore leading whitespaces
+
   let index = 0;
   while (s[index] === " ") {
     index++
   }
-  //check if the next character is '-' or '+'
+
   let sign = 1 //default to '+'
   if (s[index] === '+') {
     sign = 1
@@ -27,20 +26,20 @@ var myAtoi = function (s) {
     sign = -1
     index++
   }
-  //read string of digits into integer
+
   const digits = {}
   for (let i = 0; i < 10; i++) {
     digits[i + ""] = i
   }
   let result = 0
   for (let i = index; i < s.length; i++) {
-    if (digits[s[i]] !== undefined) { // is digit
-      result = result * 10 + digits[s[i]] //重點
+    if (digits[s[i]] !== undefined) {
+      result = result * 10 + digits[s[i]]
     } else {
       break
     }
   }
-  // clamped to max or min
+
   result = result * sign
   if (result < -1 * Math.pow(2, 31)) {
     result = -1 * Math.pow(2, 31)
