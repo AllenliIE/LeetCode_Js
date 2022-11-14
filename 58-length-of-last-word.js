efab88f79571eeaaec52185c5d5d125a4690cfed
AllenliIE <allen.li.service@gmail.com>
2022-11-14 21:15:40 +0800

    feat: modify 58-length-of-last-word.js

--- 58-length-of-last-word.js
+++ 58-length-of-last-word.js
@@ -8,6 +8,7 @@
 //   word = [a, b, c]
 // array   0  1  2
 
+// <strong>Code 1:</strong>
 var lengthOfLastWord = function (s) {
   const arr = s.trim().split(' ')
   return arr[arr.length - 1].length
@@ -24,3 +25,15 @@ return arr[arr.length - 1].length
 //arr[1].length => 'World'.length => 5
 
 </pre> */
+
+// <strong>Code 2:</strong>
+var lengthOfLastWord = function (s) {
+  if (s.length === 0) return 0
+  let arr = s.trim().split(' ')
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i].length > 0) {
+      return arr[i].length
+    }
+  }
+};
