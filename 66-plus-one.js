a0b947dbe020c19d16e6f7418aada36aa8395cbd
AllenliIE <allen.li.service@gmail.com>
2022-11-16 14:16:51 +0800

    feat: modify 66-plus-one.js

--- 66-plus-one.js
+++ 66-plus-one.js
@@ -7,6 +7,7 @@
 // (ex.注意當input的陣列大於2 ^ 53時，要使用BigInt(value)，
 // 因為JavaScript原生的Number能夠表示的最大值為2 ^ 53。)
 
+// <strong>Code 1:</strong>
 var plusOne = function (digits) {
   if (digits.length > 11) {
     let digitsNum = BigInt(digits.join(''))
@@ -30,3 +31,22 @@ Number.(digits)++ = 123 + 1 = 124
 String(digitsNum) = '124'
 return digitsString.split('') = [1,2,4]
 </pre> */
+
+// <strong>Code 2:</strong>
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++
+      return digits
+    } else {
+      digits[i] = 0
+    }
+  }
+  digits.unshift(1)
+  return digits
+};
+
+// <strong>Code 3:</strong>
+var plusOne = function (digits) {
+  return (String(BigInt(digits.join("")) + BigInt(1))).split("")
+};
