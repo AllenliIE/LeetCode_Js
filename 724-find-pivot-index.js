c0616cb310314969536a378c1ebf1c1821ab9894
AllenliIE <allen.li.service@gmail.com>
2022-11-23 17:14:24 +0800

    feat: modify 724-find-pivot-index.js

--- 724-find-pivot-index.js
+++ 724-find-pivot-index.js
@@ -1,69 +1,56 @@
-//Blog: http://52.198.119.162/2022/08/08/leetcode-js-724-find-pivot-index/
+//Blog: http://52.198.119.162/leetcode-js-724-find-pivot-index/
 
 // <strong>Solution:</strong>
-//1. 設定 i = 0, j = 0。(分別為 s 和 t 的陣列起始值)
-//2. 宣告 s 和 t 的長度為 sLength 和 tLength。
-//3. 如果 s 為空值，則返回 true。
-//4. 當 i 和 j 同時小於 s 和 t的長度時，
-//5. 如果 s 中單一字串符合 t，則進入下一個 s 單一字串。
-//6. 如果不符合，則判斷下一個 t 的單一字串。(依序比對)
-//7. 如果 i 有符合 s 的長度，則返回 ture，否則為 false。
+// 1. 先設定 sum = 0, leftSum = 0。
+// 2. 用 for 迴圈，將 nums 中的整數加總。
+// 3. 用 for 迴圈，將 sum 逐一扣除 nums 中的整數。
+// 4. 同時 if 條件，將 leftSum 與 sum做比對，符合則回傳 i(樞紐值)。
+// 5. 如否，則將 leftSum 逐一加入 nums 中的整數。(左邊望大，右邊望小逐一比對)
+// 6. 如 for 迴圈執行完沒有提前跳出，則回傳 - 1。
 
+// <strong>Code:</strong>
+var pivotIndex = function (nums) {
+  let sum = 0, leftSum = 0
 
-
-var isSubsequence = function (s, t) {
-  let i = 0, j = 0
-  let sLength = s.length, tLength = t.length
-
-  if (s.length === 0) return true
-  while (i < sLength && j < tLength) {
-    if (s[i] === t[j]) i++
-    j++
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
   }
-  return i === sLength
+  for (let i = 0; i < nums.length; i++) {
+    sum -= nums[i]
+
+    if (leftSum === sum) return i
+    leftSum += nums[i]
+  }
+  return -1
 }
 
 /*< strong > Example 1</strong >
 <pre style='background-color:#ggg'>
-Input: s = "abc", t = "ahbgdc"
-i = 0 ,j = 0
-sLength = 3, tLength = 6
+Input: nums = [1,7,3,6,5,6]
+sum = 0, leftSum = 0
+sum = 1 + 7 + 3 + 6 + 5 + 6 => 28
 
 step.1
-i < sLength //0 < 3
-j < tLength //0 < 6
-s[0] === t[0]; i++ //a === a; i = 1
-j++ //j = 1
+i = 0
+sum -= nums[i] = 28 - 1 => 27
+leftSum === sum => 0 !== 27
+leftSum += nums[i] => 0 + 1 = 1
 
 step.2
-i < sLength //1 < 3
-j < tLength //1 < 6 
-s[1] !== t[1] //b !== h
-j++ //j = 2
+i = 1
+sum -= nums[i] = 27 - 7 => 20
+leftSum === sum => 1 !== 20
+leftSum += nums[i] => 1 + 7 = 8
 
 step.3
-i < sLength //1 < 3
-j < tLength //2 < 6 
-s[1] === t[2]; i++ //b === b; i = 2
-j++ //j = 3
+i = 2
+sum -= nums[i] = 20 - 3 => 17
+leftSum === sum => 1 !== 17
+leftSum += nums[i] => 8 + 3 = 11
 
 step.4
-i < sLength //2 < 3
-j < tLength //3 < 6 
-s[2] !== t[3] //c !== g
-j++ //j = 4
-
-step.5
-i < sLength //2 < 3
-j < tLength //4 < 6
-s[2] !== t[4] //c !== d
-j++ //j = 5
-
-step.6
-i < sLength //2 < 3
-j < tLength //5 < 6
-s[2] === t[5]; i++ //c === c; i = 3
-j++ //j = 6
-
-break //i < sLength(3 = 3), j < tLength(6 = 6)
-</pre> */
+i = 3
+sum -= nums[i] = 17 - 6 => 11
+leftSum === sum => 11 === 11
+return i //i = 3
+</pre>*/
