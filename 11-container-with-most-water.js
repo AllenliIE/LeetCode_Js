3a76cf8853c93903a8b5bae0006645330a0e9851
AllenliIE <allen.li.service@gmail.com>
2022-12-07 20:22:37 +0800

    feat: modify 11-container-with-most-water.js

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -11,9 +11,10 @@
 //6. 回傳max */
 
 var maxArea = function (height) {
-  let left = 0
-  let right = height.length - 1
-  let max = (right - left) * Math.min(height[left], height[right])
+  let left = 0,
+    right = height.length - 1,
+    max = (right - left) * Math.min(height[left], height[right])
+
   while (right > left) {
     if (height[right] < height[left]) {
       right--
@@ -24,8 +25,8 @@ var maxArea = function (height) {
     if (currentMax > max) {
       max = currentMax
     }
+    return max;
   }
-  return max
 }
 
 /*< strong > Example 1</strong >
@@ -38,7 +39,7 @@ var maxArea = function (height) {
     right - left = 8 - 0 = 8
     Math.min(1, 7) = 1
     max = 8 * 1 = 8
-
+ 
     step.2
     L
     [1,8,6,2,5,4,8,3,7]
@@ -46,7 +47,7 @@ var maxArea = function (height) {
     right - left = 8 - 1 = 7
     Math.min(8, 7) = 7
     max = 7 * 7 = 49
-
+ 
     step.3
     L
     [1,8,6,2,5,4,8,3,7]
@@ -54,7 +55,7 @@ var maxArea = function (height) {
     right - left = 7 - 1 = 6
     Math.min(8, 3) = 3
     max = 6 * 3 = 18
-
+ 
     step.4
     L
     [1,8,6,2,5,4,8,3,7]
@@ -62,7 +63,7 @@ var maxArea = function (height) {
     right - left = 7 - 2 = 5
     Math.min(6, 3) = 3
     max = 5 * 3 = 15
-
+ 
     step.5
     L
     [1,8,6,2,5,4,8,3,7]
@@ -70,7 +71,7 @@ var maxArea = function (height) {
     right - left = 6 - 2 = 4
     Math.min(6, 8) = 6
     max = 4 * 6 = 24
-
+ 
     step.6
     L
     [1,8,6,2,5,4,8,3,7]
@@ -78,7 +79,7 @@ var maxArea = function (height) {
     right - left = 6 - 3 = 3
     Math.min(2, 8) = 2
     max = 3 * 2 = 6
-
+ 
     step.7
     L
     [1,8,6,2,5,4,8,3,7]
@@ -86,7 +87,7 @@ var maxArea = function (height) {
     right - left = 6 - 4 = 2
     Math.min(5, 8) = 5
     max = 1 * 5 = 5
-
+ 
     step.8
     L
     [1,8,6,2,5,4,8,3,7]
@@ -94,7 +95,7 @@ var maxArea = function (height) {
     right - left = 6 - 5 = 1
     Math.min(4, 8) = 4
     max = 1 * 4 = 4
-
+ 
     step.9
     L
     [1,8,6,2,5,4,8,3,7]
@@ -102,13 +103,14 @@ var maxArea = function (height) {
     right - left = 6 - 6 = 0
     Math.min(8, 8) = 8
     max = 0 * 8 = 0
-
+ 
     
   </pre> */
 
 //< strong > Code 2:</strong >
 var maxArea = function (height) {
-  let max = 0
+  let max = 0;
+
   for (let i = 0; i < height.length; i++) {
     for (let j = i + 1; j < height.length; j++) {
       let currentMax = (j - i) * Math.min(height[i], height[j])
@@ -117,7 +119,7 @@ var maxArea = function (height) {
       }
     }
   }
-  return max
+  return max;
 }
 
 //<strong>Code 3: Conditional (ternary) operator</strong>
