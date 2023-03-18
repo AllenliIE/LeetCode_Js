a9ab3ec82424a6dd1ca08a3ef937dac99faf9ddc
AllenliIE <allen.li.service@gmail.com>
2023-03-18 13:50:42 +0800

    feat: modify 2068-missing-number.js

--- 0268-missing-number.js
+++ 0268-missing-number.js
@@ -9,7 +9,33 @@
 //    (正常數列的加總 - 減去該陣列實際值得加總 = 缺少的數字)
 // 5. 回傳 nums 的長度減去 result。
 
-// <strong>Code 1: BigO(2n)</strongc>
+/* <strong>Code 1: BigO(n)</strongc> */
+var missingNumber = function(nums) {
+  let length = nums.length,
+      numsTotal = 0
+
+  if (length === 0) return 0;
+
+  for (let i = 0; i < length; i++) {
+    numsTotal += nums[i] - i
+  }
+
+  return length - numsTotal;
+};
+
+/* <strong>FlowChart:</strong>
+<strong>Example 1</strong>
+Input: nums = [3,0,1]
+
+numsTotal += nums[i] - i
+numsTotal = 0 + 3 - 0 = 3
+numsTotal = 3 + 0 - 1 = 2
+numsTotal = 2 + 1 - 2 = 1
+
+return length - numsTotal = 3 - 1 = 2
+*/
+
+// <strong>Code 2: BigO(2n)</strongc>
 var missingNumber = function (nums) {
   let length = nums.length
       lengthTotal = 0,
@@ -24,32 +50,6 @@ var missingNumber = function (nums) {
     numsTotal += nums[i]
   }
   return lengthTotal - numsTotal;  
-};
-
-/* <strong>FlowChart:</strong>
-<strong>Example 1</strong>
-Input: nums = [3,0,1]
-
-result += nums[i] - i
-result = 0 + 3 - 0 = 3
-result = 3 + 0 - 1 = 2
-result = 2 + 1 - 2 = 1
-
-return numLength - result = 3 - 1 = 2
-*/
-
-/* <strong>Code 2: BigO(n)</strongc> */
-var missingNumber = function(nums) {
-  let length = nums.length,
-      numsTotal = 0
-
-  if (length === 0) return 0;
-
-  for (let i = 0; i < length; i++) {
-    numsTotal += nums[i] - i
-  }
-
-  return length - numsTotal;
 };
 
 /* <strong>Code 3: BigO(n) ES6 Set</strongc> */
