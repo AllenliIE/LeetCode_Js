69ceaf7ca4409b96a85dc73b5178dc6b9e7e1e94
AllenliIE <allen.li.service@gmail.com>
2023-03-27 08:36:55 +0800

    feat: modify 0217-contains-duplicate.js

--- 0217-contains-duplicate.js
+++ 0217-contains-duplicate.js
@@ -1,19 +1,18 @@
 //Blog: http://52.198.119.162/leetcode-js-217-contains-duplicate/
 
 // <strong>Solution:</strong>
-// 1. 建立 map 物件。
+// 1. 建立 hashTable 物件。
 // 2. 運用「for of」來將 nums 陣列中的數值依序取出。
-// 3. 判斷該數值是否存在 map 物件中，如有回傳 true，反之在 map 物件存入該數值為名稱，並配對 true。
+// 3. 判斷該數值是否存在 hashTable 物件中，如有回傳 true，反之在 hashTable 物件存入該數值為名稱，並配對 true。
 // 4. 沒有重複出現的數值，則回傳 false。
 
 // <strong>Code:</strong>
 var containsDuplicate = function (nums) {
-  let map = {}
+  let hashTable = {}
 
-  for (let i of nums) {
-
-    if (map[i]) return true
-    map[i] = true
+  for (let num of nums) {
+    if (hashTable[num]) return true;
+    hashTable[num] = true
   }
   return false;
 };
@@ -27,5 +26,5 @@ Input: nums = [1,2,3,1]
 { '1': true, '2': true }
 { '1': true, '2': true, '3': true }
 
-if (map[1]) = true, return true
+if (hashTable[1]) = true, return true;
 </pre> */
