2233e51fe964e67f6bb33c9e39735d8b8a90e6b6
AllenliIE <allen.li.service@gmail.com>
2023-04-07 23:28:41 +0800

    feat: modify 0242-valid-anagram.js

--- 0242-valid-anagram.js
+++ 0242-valid-anagram.js
@@ -25,6 +25,8 @@ return true */
 
 // <strong>Code 2: BigO(n log n)</strong>
 var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
   if (s.split("").sort().join("") === t.split("").sort().join("")) {
     return true;
   } else {
