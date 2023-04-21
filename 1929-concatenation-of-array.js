4443f68f031b186fc1065e0721919e8c38730496
AllenliIE <allen.li.service@gmail.com>
2023-04-21 10:58:15 +0800

    feat: modify 1929-concatenation-of-array.js

--- 1929-concatenation-of-array.js
+++ 1929-concatenation-of-array.js
@@ -6,22 +6,17 @@
 // 3. 將 for (j < nums.length)將每個 nums 的元素執行一次，並運用.push() 放入 ans。
 // 4. 回傳 ans。
 
-// <strong>Code.1: For Loop</strong>
+// <strong>Code.1: For Loop (BigO(n^2)</strong>
 var getConcatenation = function (nums) {
-  let ans = []
+  let array = [];
 
   for (let i = 0; i < 2; i++) {
     for (let j = 0; j < nums.length; j++) {
-      ans.push(nums[j])
+      array.push(nums[j]);
     }
   }
-  return ans
-}
-
-// <strong>Code.2: .Concat()</strong>
-var getConcatenation = function (nums) {
-  return nums.concat(nums)
-}
+  return array;
+};
 
 /* <strong>Example 1</strong>
 Input: nums = [1,2,1]
@@ -39,3 +34,17 @@ ans.push(nums[1]) => [1, 2, 1, 1, 2]
 ans.push(nums[2]) => [1, 2, 1, 1, 2, 1]
 
 return ans //[1, 2, 1, 1, 2, 1] */
+
+// <strong>Code.2: Concat (BigO(n)</strong>
+var getConcatenation = function (nums) {
+  return nums.concat(nums);
+};
+
+// <strong>Code.3: for loop (BigO(n))</strong>
+var getConcatenation = function (nums) {
+  const length = nums.length;
+  for (let i = 0; i < length; i++) {
+    nums.push(nums[i]);
+  }
+  return nums;
+};
