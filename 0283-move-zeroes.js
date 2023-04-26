c1c1cf0e8bc6de7bdf4bd382e8eaeef7227e4dba
AllenliIE <allen.li.service@gmail.com>
2023-04-27 01:32:35 +0800

    feat: modify 0283-move-zeros.js

--- 0283-move-zeroes.js
+++ 0283-move-zeroes.js
@@ -1,27 +1,29 @@
-//Blog: http://52.198.119.162/leetcode-js-283-move-zeroes/
+//Blog: http://allenliservice.site/leetcode-js-283-move-zeroes/
 
 // <strong>Solution:</strong>
-// 1. 宣告 j = 0。
+// 1. 宣告 index = 0。
 // 2. 執行 i 長度的迴圈。
 // 3. 如果陣列中的數值不是 0，則前後交換位置。
-// 4. 如果 i 是 0 的話，則不變動，往下搜尋，並由 j 來將 0 替換至後方。
+// 4. 如果 i 是 0 的話，則不變動，往下搜尋，並由 index 來將 0 替換至後方。
 
 // <strong>Code 1:</strongc>
 var moveZeroes = function (nums) {
-  let j = 0
+  let index = 0;
+
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      [nums[i], nums[j]] = [nums[j], nums[i]]
-      j++
+      [nums[i], nums[index]] = [nums[index], nums[i]];
+      index++;
     }
   }
-  return nums
+
+  return nums;
 };
 
 /* <strong>FlowChart:</strong>
 <strong>Example 1</strong>
 Input: nums = [0,1,0,3,12]
-i, j, nums[i], nums[j]
+i, index, nums[i], nums[index]
 
 0 0 0 0
 1 0 1 0
