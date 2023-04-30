5d76eafd826024a806f7448ef05d63af370f3ac2
AllenliIE <allen.li.service@gmail.com>
2023-04-30 20:33:02 +0800

    feat: modify 0283-move-zeroes.js

--- 0283-move-zeroes.js
+++ 0283-move-zeroes.js
@@ -5,8 +5,9 @@
 // 2. 執行 i 長度的迴圈。
 // 3. 如果陣列中的數值不是 0，則前後交換位置。
 // 4. 如果 i 是 0 的話，則不變動，往下搜尋，並由 index 來將 0 替換至後方。
+// 5. 回傳 nums。
 
-// <strong>Code 1:</strongc>
+// <strong>Code 1: BigO(n)</strongc>
 var moveZeroes = function (nums) {
   let index = 0;
 
@@ -32,3 +33,17 @@ i, index, nums[i], nums[index]
 4 2 12 0
 [ 1, 3, 12, 0, 0 ]
 */
+
+// <strong>Code 2: BigO(n^2)</strongc>
+var moveZeroes = function (nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === 0) {
+        let box = nums[i];
+        nums[i] = nums[j];
+        nums[j] = box;
+      }
+    }
+  }
+  return nums;
+};
