77e83dc3e560ccdd5fa9c38431ea0c2817df87d1
AllenliIE <allen.li.service@gmail.com>
2023-05-05 10:51:10 +0800

    feat: modify 0088-merge-sorted-array.js

--- 0088.merge-sorted-array.js
+++ 0088.merge-sorted-array.js
@@ -106,3 +106,28 @@ var merge = function (nums1, m, nums2, n) {
 
   return nums1.sort((a, b) => a - b);
 };
+
+<strong>Code 5: BigO(n)</strong>;
+var merge = function (nums1, m, nums2, n) {
+  // let index of completed nums1
+  let k = m + n - 1;
+
+  // merge in reverse order
+  while (m > 0 && n > 0) {
+    if (nums1[m - 1] > nums2[n - 1]) {
+      nums1[k] = nums1[m - 1];
+      m--;
+    } else {
+      nums1[k] = nums2[n - 1];
+      n--;
+    }
+    k--;
+  }
+
+  // if m = 0 then push nums2 in nums1
+  while (n > 0) {
+    nums1[k] = nums2[n - 1];
+    k--;
+    n--;
+  }
+};
