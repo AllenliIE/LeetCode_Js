28079b35b9a95c262c2e74bc5b19c1b2b77b5898
AllenliIE <allen.li.service@gmail.com>
2023-05-11 16:54:32 +0800

    feat: create 2629-function-composition.js

---
+++ 2629-function-composition.js
@@ -0,0 +1,45 @@
+//Blog: https://allenliservice.site/leetcode-javascript-30-days-challenge-day7-2629-function-composition/
+
+// <strong>solution:</strong>
+// 這題最重要的是了解題目的說明，題目將條件依序放到陣列中，
+// 同時希望我們由右到左的讀取陣列函式的內容，
+// 這個過程我們可以使用reduceRight()來完成指定功能，如下：
+
+// <strong>Code 1: BigO(n)</strong>
+var compose = function (functions) {
+  return function (x) {
+    return functions.reduceRight((acc, cur) => cur(acc), x);
+  };
+};
+
+/* <strong>FlowChart:</strong>
+<strong>Example 1</strong>
+<pre style='background-color:#ggg'>
+Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+functions.reduceRight((acc, cur) => cur(acc), x)
+
+functions.reduceRight((acc, cur) => cur(2 * 4), 4) //acc = 8
+functions.reduceRight((acc, cur) => cur(8 * 8), 8) //acc = 64
+functions.reduceRight((acc, cur) => cur(64 + 1), 64) //acc = 65
+return 65;
+</pre>
+
+<strong>Example 2</strong>
+<pre style='background-color:#ggg'>
+Input: functions = [x => 10 * x, x => 10 * x, x => 10 * x], x = 1
+functions.reduceRight((acc, cur) => cur(acc), x)
+
+functions.reduceRight((acc, cur) => cur(10 * 1), 1) //acc = 10
+functions.reduceRight((acc, cur) => cur(10 * 10), 10) //acc = 100
+functions.reduceRight((acc, cur) => cur(10 + 100), 100) //acc = 1000
+return 1000;
+</pre>
+
+<strong>Example 3</strong>
+<pre style='background-color:#ggg'>
+Input: functions = [], x = 42
+functions.reduceRight((acc, cur) => cur(acc), x)
+
+functions.reduceRight((acc, cur) => cur(acc), 42)
+return 42;
+</pre> */
