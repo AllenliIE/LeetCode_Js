8e8a7be7eee93e828254431d16b3a57bfbee0b53
AllenliIE <allen.li.service@gmail.com>
2023-06-11 20:50:58 +0800

    feat: modify 0219-contains-duplicate-ii.js

--- 0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii.js
@@ -58,10 +58,10 @@ const containsNearbyDuplicate = function (nums, k) {
   if (nums.length <= 1) return false;
   const set = new Set();
   for (let i = 0; i < nums.length; i++) {
-    if (set.has(nums[i])) {
-      return true;
-    }
+    if (set.has(nums[i])) return true;
+
     set.add(nums[i]);
+    
     if (set.size > k) {
       set.delete(nums[i - k]);
     }
