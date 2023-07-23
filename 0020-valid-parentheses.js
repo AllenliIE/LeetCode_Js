0e35b8dd2083e3c4f93f31d96862bf44955a2132
AllenliIE <allen.li.service@gmail.com>
2023-07-23 14:30:36 +0800

    feat: modify 0020-valid-parentheses.js

--- 0020-valid-parentheses.js
+++ 0020-valid-parentheses.js
@@ -1,28 +1,28 @@
-//Blog:http://52.198.119.162/2022/06/07/leetcode-js-20-valid-parentheses/
+//Blog:https://www.allenliservice.site/leetcode-js-20-valid-parentheses/
 
 //<strong>Solution:</strong>
 // 1. 運用「for...of」將input進行迴圈，比對錯誤即回傳false
 // 2. 當「if條件」符合左括號時，push()放入空陣列中
 // 3. 當「if條件」出現右括號時，pop()比對上一次儲存的左括號
-// 4. 最後當宣告的空陣列長度為0時，回傳ture 
+// 4. 最後當宣告的空陣列長度為0時，回傳ture
 
 // <strong>Code 1: BigO(n)</strong>
 var isValid = function (s) {
-  const array = []
+  const array = [];
   const map = {
-      ")":"(",
-      "]":"[",
-      "}":"{"
-  }
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
 
   for (let char of s) {
-      if (char === "(" || char === "[" || char === "{") {
-          array.push(char)
-      } else if (array.pop() !== map[char]) {
-          return false;
-      }
+    if (char === "(" || char === "[" || char === "{") {
+      array.push(char);
+    } else if (array.pop() !== map[char]) {
+      return false;
+    }
   }
-  return (!array.length);
+  return !array.length;
 };
 
 /* <strong>Example 1</strong>
@@ -39,18 +39,18 @@ array.length === 0 => true
 
 // <strong>Code 2: BigO(n)</strong>
 var isValid = function (s) {
-  const array = []
+  const array = [];
 
   for (i = 0; i < s.length; i++) {
-    if (s[i] === '(') {
-      array.push(')')
-    } else if (s[i] === '{') {
-      array.push('}')
-    } else if (s[i] === '[') {
-      array.push(']')
+    if (s[i] === "(") {
+      array.push(")");
+    } else if (s[i] === "{") {
+      array.push("}");
+    } else if (s[i] === "[") {
+      array.push("]");
     } else if (array.pop() !== s[i]) {
       return false;
     }
   }
-  return (array.length === 0);
+  return array.length === 0;
 };
