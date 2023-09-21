5534905ec1779ff658406db52081919b20faa558
AllenliIE <allen.li.service@gmail.com>
2023-09-21 09:27:49 +0800

    feat: modify 1929-concatenation-of-array.js

--- 1929-concatenation-of-array.js
+++ 1929-concatenation-of-array.js
@@ -1,4 +1,4 @@
-//Blog: http://52.198.119.162/leetcode-js-1929-concatenation-of-array/
+//Blog: https://www.allenliservice.site/leetcode-js-1929-concatenation-of-array/
 
 // <strong>Solution:</strong>
 // 1. 建立 ans = []
