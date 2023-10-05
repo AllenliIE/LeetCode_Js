f88291cdb40d5d3f5a7ca4de8ba177c611016906
AllenliIE <allen.li.service@gmail.com>
2023-10-05 09:16:59 +0800

    feat: modify 0055-jump-game.js

--- 0055-jump-game.js
+++ 0055-jump-game.js
@@ -9,3 +9,20 @@ var canJump = function (nums) {
   }
   return position === 0;
 };
+
+//DP
+var canJump = function (nums) {
+  let n = nums.length;
+  let dp = new Array(n).fill(false);
+
+  dp[0] = true;
+
+  for (let i = 0; i < n; i++) {
+    if (dp[i]) {
+      for (let j = 1; j <= nums[i] && i + jump < n; jump++) {
+        dp[i + jump] = true;
+      }
+    }
+  }
+  return dp[n - 1];
+};
