6c4304ff5ffa195957e9496a2b60f507d54d5439
AllenliIE <allen.li.service@gmail.com>
2023-10-06 09:25:37 +0800

    feat: modify 0055-jump-game.js

--- 0055-jump-game.js
+++ 0055-jump-game.js
@@ -1,16 +1,4 @@
-//Greedy
-var canJump = function (nums) {
-  let position = nums.length - 1;
-
-  for (let i = position; i >= 0; i--) {
-    if (i + nums[i] >= position) {
-      position = i;
-    }
-  }
-  return position === 0;
-};
-
-//DP
+// <strong>Code 1: DP BigO(n ^ 2)</strong>
 var canJump = function (nums) {
   let n = nums.length;
   let dp = new Array(n).fill(false);
@@ -25,4 +13,16 @@ var canJump = function (nums) {
     }
   }
   return dp[n - 1];
+};
+
+// <strong>Code 2: Greedy BigO(n)</strong>
+var canJump = function (nums) {
+  let position = nums.length - 1;
+
+  for (let i = position; i >= 0; i--) {
+    if (i + nums[i] >= position) {
+      position = i;
+    }
+  }
+  return position === 0;
 };
