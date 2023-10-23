21a430154c3f2d5db0ad6fca40507b3c959f6420
AllenliIE <allen.li.service@gmail.com>
2023-10-23 09:13:52 +0800

    feat: modify 0028-implement-strStr().js

--- 0028-implement-strStr().js
+++ 0028-implement-strStr().js
@@ -1,12 +1,12 @@
 //Blog:https://www.allenliservice.site/leetcode-js-28-implement-strstr/
 
 //<strong>Solution:</strong>
-//1. if「needle」是空值，回傳-1
-//2. if「haystack」是空值或「needle.length」比「haystack.length」大，回傳 - 1
-//3. for迴圈跑haystack的長度，並運用.substr()帶入neeedle進行比較
-//ex.類似excel的MID函式，但.substr()會取陣列起點值，.substring()會取陣列終點值。
-//(A1 = ABCDEF; =MID(A1, 2, 1); B )
-//4.正確回傳陣列起點值，錯誤回傳 - 1
+// 1. if「needle」是空值，回傳0
+// 2. if「haystack」是空值或「needle.length」比「haystack.length」大，回傳-1
+// 3. for迴圈跑haystack的長度，並運用.slice()帶入neeedle進行比較
+// ex.類似excel的MID函式，而.substr()會取陣列起點值，.substring()會取陣列終點值。
+// (A1 = ABCDEF; =MID(A1,2,1); B )
+// 4.正確回傳陣列起點值，錯誤回傳-1
 
 var strStr = function (haystack, needle) {
   if (!needle) return -1;
